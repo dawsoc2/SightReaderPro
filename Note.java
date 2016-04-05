50f41ae4e1c4da69b4bcd5aa4de782aa034e2400
vargaa4 <vargaa4@rpi.edu>
2016-04-04 21:09:23 -0400

    Modified Song constructor to construct song data
    The Song constructor now will create an ArrayList of Notes to represent
    the song. Some minor things in both Song and Note classes were also
    modified.

--- Note.java
+++ Note.java
@@ -3,13 +3,19 @@ package Song;
 class Note {
 	
 	private int staff_position;
-	private String note_type;
+	private char note_type;
 	private char modifier;
 	private char clef;
 	
-	public Note(int new_staff_position, String new_note_type, char new_modifier, char new_clef) {
-		staff_position = new_staff_position;
-		note_type = new_note_type;
+	public Note(String note_properties, char new_modifier, char new_clef) {
+		note_type = note_properties.charAt(0);
+		String temp_position = note_properties.substring(1);
+		if (temp_position.equals("R")){
+			staff_position = -20;
+		}else{
+			staff_position = Integer.parseInt(temp_position);
+		}
+		
 		modifier = new_modifier;
 		clef = new_clef;
 	}
@@ -33,50 +39,54 @@ class Note {
 		
 		String note_text = "";
 		
-		if (note_type.charAt(1) == 'n') {								// Note is not a rest
+		if (staff_position > -20) {						// Note is not a rest
 			note_text += '\u8080';								// 0 time elapsed (first 2 bytes)
 			note_text += '\u0090';								// 0 time elapsed; note ON, channel 1
 			note_text += (char)((staff_position<<8)&0xFF00 + 0x40);	// note num and velocity (use 40)
 		
 			int wait_time = 0;
 			// Add to this list when we add more supported note lengths
-			if (note_type.equals("qn")) {							// Quarter note
+			if (note_type == 'Q') {							// Quarter note
 				wait_time = 96;									// Wait one beat
-			} else if (note_type.equals("hn")) {						// Half note
+			} else if (note_type == 'H') {						// Half note
 				wait_time = 192;								// Wait 2 beats
-			} else if (note_type.equals("wn")) {						// Whole note
+			} else if (note_type == 'W') {						// Whole note
 				wait_time = 384;								// Wait 4 beats
+			}else{
+				System.out.println("ERROR: UNRECOGNIZED NOTE TYPE!");
 			}
 
-                        // fuck variable size numbers
-                        char w1 = (char) ((char)((wait_time>>14)&0x7F) | 0x80);
-                        char w2 = (char) ((char)((wait_time>>7)&0x7F) | 0x80);
-                        char w3 = (char)(wait_time&0x7F);
-                        
-                        note_text += (char)(w1<<8 + w2);                        // wait_time elapsed (first 2 bytes)
-                        note_text += (char)(w3<<8 + 0x90);                      // wait_time elapsed; note ON, channel 1
+			// fuck variable size numbers
+			char w1 = (char) ((char)((wait_time>>14)&0x7F) | 0x80);
+			char w2 = (char) ((char)((wait_time>>7)&0x7F) | 0x80);
+			char w3 = (char)(wait_time&0x7F);
+			
+			note_text += (char)(w1<<8 + w2);                        // wait_time elapsed (first 2 bytes)
+			note_text += (char)(w3<<8 + 0x90);                      // wait_time elapsed; note ON, channel 1
 			note_text += (char)((staff_position<<8)&0xFF00 + 0x40);	// note num and velocity (40)
 		}
 		
 		else {													// Note is a rest
 			int wait_time = 0;
 			// Add to this list when we add more supported rest lengths
-			if (note_type.equals("qr")) {							// Quarter rest
+			if (note_type == 'Q') {							// Quarter rest
 				wait_time = 96;									// Wait one beat
-			} else if (note_type.equals("hr")) {						// Half rest
+			} else if (note_type == 'H') {						// Half rest
 				wait_time = 192;								// Wait 2 beats
-			} else if (note_type.equals("wr")) {						// Whole rest
+			} else if (note_type == 'W') {						// Whole rest
 				wait_time = 384;								// Wait 4 beats
+			}else{
+				System.out.println("ERROR: UNRECOGNIZED REST TYPE!");
 			}
 			
-                        // fuck variable size numbers
-                        char w1 = (char) ((char)((wait_time>>14)&0x7F) | 0x80);
-                        char w2 = (char) ((char)((wait_time>>7)&0x7F) | 0x80);
-                        char w3 = (char)(wait_time&0x7F);
-                        
-                        note_text += (char)(w1<<8 + w2);                        // wait_time elapsed (first 2 bytes)
-                        note_text += (char)(w3<<8 + 0x90);                      // wait_time elapsed; note ON, channel 1
-                        note_text += '\u0040';								// note num (0) and velocity (40)
+			// fuck variable size numbers
+			char w1 = (char) ((char)((wait_time>>14)&0x7F) | 0x80);
+			char w2 = (char) ((char)((wait_time>>7)&0x7F) | 0x80);
+			char w3 = (char)(wait_time&0x7F);
+			
+			note_text += (char)(w1<<8 + w2);                        // wait_time elapsed (first 2 bytes)
+			note_text += (char)(w3<<8 + 0x90);                      // wait_time elapsed; note ON, channel 1
+			note_text += '\u0040';								// note num (0) and velocity (40)
 			
 			note_text += '\u0000';								// 0 time elapsed (first 2 bytes)
 			note_text += '\u0080';								// 0 time elapsed; note OFF, channel 1
