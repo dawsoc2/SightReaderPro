aa4d082412cfeac7119a53a91fd17d68db8ae3b4
alexreimcd <alexreimcd@gmail.com>
2016-04-07 01:48:43 -0400

    Added drop down menus to properties page
    -added spinners
    -added values for spinners through Java so values not visible in xml

--- SightReader Pro/app/src/main/java/com/example/xerxes/cameratest/PropertiesPage.java
+++ SightReader Pro/app/src/main/java/com/example/xerxes/cameratest/PropertiesPage.java
@@ -9,7 +9,9 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class PropertiesPage extends AppCompatActivity {
 
@@ -20,6 +22,27 @@ public class PropertiesPage extends AppCompatActivity {
 
         //set variables
         ImageView photoDisplay = (ImageView)findViewById(R.id.prop_imageView);
+        //create clef spinner
+        String clefArray[] = {"Treble", "Bass"};
+        Spinner spinnerClef = (Spinner) findViewById(R.id.spinnerClef);
+        ArrayAdapter<String> clefsSpinnerArrayAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, clefArray);
+        clefsSpinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        spinnerClef.setAdapter(clefsSpinnerArrayAdapter);
+        //spinnerClef.setSelection(getIndex(spinnerClef, "Treble"));
+
+        //create key signature spinner
+        String keySigArray[] = {"B/C♭", "E", "A", "D", "G", "C", "F", "B♭", "E♭", "A♭", "D♭/C♯", "G♭/F♯"};
+        Spinner spinnerKeySig = (Spinner) findViewById(R.id.spinnerKeySignature);
+        ArrayAdapter<String> keySigSpinnerArrayAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, keySigArray);
+        keySigSpinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        spinnerKeySig.setAdapter(keySigSpinnerArrayAdapter);
+
+        //create instrument key spinner
+        String instKeyArray[] = {"B/C♭", "E", "A", "D", "G", "C", "F", "B♭", "E♭", "A♭", "D♭/C♯", "G♭/F♯"};
+        Spinner spinnerInst = (Spinner) findViewById(R.id.spinnerInstrument);
+        ArrayAdapter<String> instKeySpinnerArrayAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, instKeyArray);
+        instKeySpinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        spinnerInst.setAdapter(instKeySpinnerArrayAdapter);
 
         //dummy intent just to get variables passed
         Intent intent = getIntent();
