abdfee601c0f72a28edf254d64ddf75442849822
Chandler Xerxes Dawson <dawsoc2@hotmail.com>
2016-05-08 18:50:04 -0400

    Properties page fixes
    removed unnecessary button, refactored

--- SightReaderPro/app/src/main/java/com/example/xerxes/cameratest/PropertiesPage.java
+++ SightReaderPro/app/src/main/java/com/example/xerxes/cameratest/PropertiesPage.java
@@ -3,13 +3,10 @@ package com.example.xerxes.cameratest;
 //android imports
 import android.content.Intent;
 import android.content.res.AssetManager;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,21 +24,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.*;
 
 //android-midi imports
 import com.googlecode.tesseract.android.TessBaseAPI;
 import com.leff.midi.*;
-import com.leff.midi.util.*;
-import com.leff.midi.event.*;
-import com.leff.midi.event.meta.*;
 
 //song imports
 import com.example.xerxes.cameratest.Song.*;
 
 //misc. imports
-import com.example.xerxes.cameratest.InputFilterMinMax;
-import com.example.xerxes.cameratest.BlobCut;
+
 
 public class PropertiesPage extends AppCompatActivity {
 
@@ -54,12 +46,12 @@ public class PropertiesPage extends AppCompatActivity {
 
         //dummy intent just to get variables passed
         Intent intent = getIntent();
-        String imagePath = intent.getStringExtra("imagePath");
+        String image_path = intent.getStringExtra("image_path");
 
         //load bitmap
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        final Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
+        final Bitmap bitmap = BitmapFactory.decodeFile(image_path, options);
 
         //set variables
         final ImageView photoDisplay = (ImageView)findViewById(R.id.prop_imageView);
@@ -73,51 +65,55 @@ public class PropertiesPage extends AppCompatActivity {
 
         //create clef spinner
         String clefArray[] = {"Treble", "Bass"};
-        final Spinner spinnerClef = (Spinner) findViewById(R.id.spinnerClef);
-        ArrayAdapter<String> clefsSpinnerArrayAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, clefArray);
-        clefsSpinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
-        spinnerClef.setAdapter(clefsSpinnerArrayAdapter);
-        //spinnerClef.setSelection(getIndex(spinnerClef, "Treble"));
-        int clefSpinnerPosition = clefsSpinnerArrayAdapter.getPosition("Treble");
-        spinnerClef.setSelection(clefSpinnerPosition);
+        final Spinner spinner_clef = (Spinner) findViewById(R.id.spinnerClef);
+        ArrayAdapter<String> clefs_spinner_array_adapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, clefArray);
+        clefs_spinner_array_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        spinner_clef.setAdapter(clefs_spinner_array_adapter);
+        //spinner_clef.setSelection(getIndex(spinner_clef, "Treble"));
+        int clef_spinner_position = clefs_spinner_array_adapter.getPosition("Treble");
+        spinner_clef.setSelection(clef_spinner_position);
 
         //create key signature spinner
         String keySigArray[] = {"B/C♭", "E", "A", "D", "G", "C", "F", "B♭", "E♭", "A♭", "D♭/C♯", "G♭/F♯"};
-        final Spinner spinnerKeySig = (Spinner) findViewById(R.id.spinnerKeySignature);
-        ArrayAdapter<String> keySigSpinnerArrayAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, keySigArray);
-        keySigSpinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
-        spinnerKeySig.setAdapter(keySigSpinnerArrayAdapter);
-        int keySigSpinnerPosition = keySigSpinnerArrayAdapter.getPosition("C");
-        spinnerKeySig.setSelection(keySigSpinnerPosition);
+        final Spinner spinner_key_sig = (Spinner) findViewById(R.id.spinnerKeySignature);
+        ArrayAdapter<String> key_sig_spinner_array_adapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, keySigArray);
+        key_sig_spinner_array_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        spinner_key_sig.setAdapter(key_sig_spinner_array_adapter);
+        int key_sig_spinner_position = key_sig_spinner_array_adapter.getPosition("C");
+        spinner_key_sig.setSelection(key_sig_spinner_position);
 
         //create instrument key spinner
         String instKeyArray[] = {"B/C♭", "E", "A", "D", "G", "C", "F", "B♭", "E♭", "A♭", "D♭/C♯", "G♭/F♯"};
-        final Spinner spinnerInst = (Spinner) findViewById(R.id.spinnerInstrument);
-        ArrayAdapter<String> instKeySpinnerArrayAdapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, instKeyArray);
-        instKeySpinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
-        spinnerInst.setAdapter(instKeySpinnerArrayAdapter);
-        int instKeySpinnerPosition = instKeySpinnerArrayAdapter.getPosition("C");
-        spinnerInst.setSelection(instKeySpinnerPosition);
+        final Spinner spinner_inst = (Spinner) findViewById(R.id.spinnerInstrument);
+        ArrayAdapter<String> inst_key_spinner_array_adapter = new ArrayAdapter<String>(this,   android.R.layout.simple_spinner_item, instKeyArray);
+        inst_key_spinner_array_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        spinner_inst.setAdapter(inst_key_spinner_array_adapter);
+        int inst_key_spinner_position = inst_key_spinner_array_adapter.getPosition("C");
+        spinner_inst.setSelection(inst_key_spinner_position);
 
         //create tempo text box
-        final EditText tempoEditText = (EditText)findViewById(R.id.editTempo);
-        tempoEditText.setFilters(new InputFilter[]{ new InputFilterMinMax("1", "1000")});
-        tempoEditText.setText("120");
+        final EditText tempo_edit_text = (EditText)findViewById(R.id.editTempo);
+        tempo_edit_text.setFilters(new InputFilter[]{new InputFilterMinMax("1", "1000")});
+        tempo_edit_text.setText("120");
 
-        final EditText songEditText = (EditText)findViewById(R.id.editSong);
+        final EditText song_edit_text = (EditText)findViewById(R.id.editSong);
 
         //create process button that will read data from the spinners
-        final Button procButton = (Button) findViewById(R.id.processButton);
-        procButton.setOnClickListener(new View.OnClickListener() {
+        final Button proc_button = (Button) findViewById(R.id.processButton);
+        proc_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String clefVal = spinnerClef.getSelectedItem().toString();
-                String keyVal = spinnerKeySig.getSelectedItem().toString();
-                String instVal = spinnerInst.getSelectedItem().toString();
-                String songVal = songEditText.getText().toString();
-                int text_tempo = Integer.parseInt(tempoEditText.getText().toString());
+                String clefVal = spinner_clef.getSelectedItem().toString();
+                String keyVal = spinner_key_sig.getSelectedItem().toString();
+                String instVal = spinner_inst.getSelectedItem().toString();
+                String songVal = song_edit_text.getText().toString();
+                int text_tempo = Integer.parseInt(tempo_edit_text.getText().toString());
+
+                //blob the bitmap
+                BlobCut blobber = new BlobCut();
+                Bitmap blobbed_bitmap = blobber.blob_cut(bitmap);
 
                 String TAG = "SightReaderPro";
-                String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+                String[] paths = new String[]{DATA_PATH, DATA_PATH + "tessdata/"};
 
                 for (String path : paths) {
                     File dir = new File(path);
@@ -139,8 +135,8 @@ public class PropertiesPage extends AppCompatActivity {
                 if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
                     try {
 
-                        AssetManager assetManager = getAssets();
-                        InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+                        AssetManager asset_manager = getAssets();
+                        InputStream in = asset_manager.open("tessdata/" + lang + ".traineddata");
                         //GZIPInputStream gin = new GZIPInputStream(in);
                         OutputStream out = new FileOutputStream(DATA_PATH
                                 + "tessdata/" + lang + ".traineddata");
@@ -170,18 +166,18 @@ public class PropertiesPage extends AppCompatActivity {
                 TessBaseAPI baseApi = new TessBaseAPI();            // tesseract object
                 baseApi.setDebug(true);
                 baseApi.init(DATA_PATH, lang);                      //DATA_PATH = our app. lang = "mus".
-                baseApi.setImage(bitmap);                           //loads bitmap into the object
+                baseApi.setImage(blobbed_bitmap);                           //loads bitmap into the object
 
-                String recognizedText = baseApi.getUTF8Text();      //get that sweet sweet text
+                String recognized_text = baseApi.getUTF8Text();      //get that sweet sweet text
 
                 baseApi.end();                                      //close the object
 
                 //generate a new song
-                Song image_song = new Song(recognizedText);
+                Song image_song = new Song(recognized_text);
                 image_song.change_tempo(text_tempo);                //set tempo, pretty standard
 
-                Log.v(TAG, "STRING: '" + recognizedText + "'\n");
-                songEditText.setText(recognizedText.replaceAll("\\s", ""));
+                Log.v(TAG, "STRING: '" + recognized_text + "'\n");
+                song_edit_text.setText(recognized_text.replaceAll("\\s", ""));
 
                 //force the song to give us a midi file
                 MidiFile midi = image_song.convert_to_midi();
@@ -205,57 +201,9 @@ public class PropertiesPage extends AppCompatActivity {
             }
         });
 
-        final Button maryButton = (Button) findViewById(R.id.maryButton);
-        maryButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                int text_tempo = Integer.parseInt(tempoEditText.getText().toString());
-                String songVal = songEditText.getText().toString();
-                songEditText.setText(songVal.replaceAll("\\s", ""));
-
-                //blob the bitmap
-                BlobCut blobber = new BlobCut();
-                Bitmap blobbed_bitmap = blobber.blob_cut(bitmap);
-                photoDisplay.setImageBitmap(blobbed_bitmap);
-                /*
-
-                */
-/*
-
-                //let's actually take that tempo and create a test midi file.
-                //first we need a song. Let's use "Mary had a lamb"
-                String mary = "Q2_ Q1_ Q0_ Q1_ Q2_ Q2_ Q2_ QR_ Q1_ Q1_ Q1_ QR_ Q2_ Q4_ H4_ Q2_ Q1_ Q0_ Q1_ Q2_ Q2_ Q2_ Q2_ Q1_ Q1_ Q2_ Q1_ H0_ HR_"; //pretty sure this is right.
-
-                //now create a Song object
-                Song mary_song = new Song(mary);
-                mary_song.change_tempo(text_tempo);
-
-                //force that object into providing a MidiFile for us.
-
-                MidiFile midi = mary_song.convert_to_midi();
-
-                // Write the MIDI data to a file
-                File output = new File("sdcard/mary_had_a_little_lamb.mid");
-                try {
-                    midi.writeToFile(output);
-                } catch (IOException e) {
-                    System.err.println(e);
-                }
-
-
-                try {
-                    mp.reset();
-                    mp.setDataSource("sdcard/mary_had_a_little_lamb.mid");
-                    mp.prepare();
-                    mp.start();
-                } catch (Exception e) {
-                }
-*/
-            }
-        });
-
         //creates back button that will take it back to the first page
-        final Button backButton = (Button) findViewById(R.id.prop_back_btn);
-        backButton.setOnClickListener(new View.OnClickListener() {
+        final Button back_button = (Button) findViewById(R.id.prop_back_btn);
+        back_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(PropertiesPage.this, MainActivity.class));
             }
