27ef73f4a02631661656936a91326bc1dc56bd53
Chandler Xerxes Dawson <dawsoc2@hotmail.com>
2016-05-08 18:54:54 -0400

    Main activity refactoring

--- SightReaderPro/app/src/main/java/com/example/xerxes/cameratest/MainActivity.java
+++ SightReaderPro/app/src/main/java/com/example/xerxes/cameratest/MainActivity.java
@@ -4,12 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -60,18 +57,18 @@ public class MainActivity extends AppCompatActivity {
     //Function for the "take photo" button.
     public void btnTakePhoto(View v) {
         //Intent to take a photo with the camera
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         //actually take the photo
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        startActivityForResult(camera_intent, CAMERA_REQUEST);
     }
 
     //Function for the "Pick Photo" button
     public void btnSelectPhoto(View v) {
         //intent to "pick" something
-        Intent selectIntent = new Intent (Intent.ACTION_PICK);
+        Intent select_intent = new Intent (Intent.ACTION_PICK);
         //we're picking an image.
-        selectIntent.setType("image/*");
-        startActivityForResult(selectIntent, PICKER_REQUEST);
+        select_intent.setType("image/*");
+        startActivityForResult(select_intent, PICKER_REQUEST);
     }
 
     public void btnTestProp(View v) {
@@ -83,15 +80,15 @@ public class MainActivity extends AppCompatActivity {
     //override "onActivityRequest" to do something special with the "startActivity"
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Intent toPropertiesPage = new Intent(MainActivity.this, PropertiesPage.class);
+        Intent to_properties_page = new Intent(MainActivity.this, PropertiesPage.class);
         if (resultCode == RESULT_OK) {
             //"take photo". Gets a bitmap directly from the camera.
             if (requestCode == CAMERA_REQUEST) {
-                Bitmap cameraImage = (Bitmap)data.getExtras().get("data");
-                Uri tempUri = getImageUri(getApplicationContext(), cameraImage);
-                String imagePath = getRealPathFromURI(tempUri);
-                toPropertiesPage.putExtra("imagePath", imagePath);
-                startActivity(toPropertiesPage);
+                Bitmap camera_image = (Bitmap)data.getExtras().get("data");
+                Uri temp_uri = getImageUri(getApplicationContext(), camera_image);
+                String image_path = getRealPathFromURI(temp_uri);
+                to_properties_page.putExtra("image_path", image_path);
+                startActivity(to_properties_page);
             }
             //"pick photo" Gets a file path that we have to extract a bitmap from.
             if (requestCode == PICKER_REQUEST) {
@@ -99,9 +96,9 @@ public class MainActivity extends AppCompatActivity {
                 String[] filePath = { MediaStore.Images.Media.DATA };
                 Cursor cursor = getContentResolver().query(pickerImage, filePath, null, null, null);
                 cursor.moveToFirst();
-                String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
-                toPropertiesPage.putExtra("imagePath", imagePath);
-                startActivity(toPropertiesPage);
+                String image_path = cursor.getString(cursor.getColumnIndex(filePath[0]));
+                to_properties_page.putExtra("image_path", image_path);
+                startActivity(to_properties_page);
                 cursor.close();
             }
         }
